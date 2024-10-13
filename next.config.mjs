85ad97ac3e37de40985c41f0a442ad19be3c5be5
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-13 22:57:00 +0000

    redoing configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-        };
-      }
-  
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
