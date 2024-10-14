bd7dbdcaa20058da6d1faea0cbb7541426793d6a
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-10-14 19:27:29 +0000

    more fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Home from '../src/components/home'
 import About from '../src/components/about'
 import Techs from '../src/components/techs';
-import Contacts from '../src/components/contacts';
+import Contacts from '../src/components/Contacts';
 
 const Index = () => {
     return (
